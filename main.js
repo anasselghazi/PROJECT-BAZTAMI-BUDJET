1fe59eee31a68312b984f98cbbd2b3b4e7a531bd
anasselghazi <anasseelghazi10@gmail.com>
2025-10-30 09:07:43 +0100

    ajouter un partie de js

---
+++ main.js
@@ -0,0 +1,11 @@
+ // LES VARIABLES
+const form = document.getElementById("transaction-form");
+const descriptionInput = document.getElementById("description");
+const amountInput = document.getElementById("amount");
+const typeInput = document.getElementById("type");
+const dateInput = document.getElementById("date");
+const transactionsList = document.getElementById("transactions-list");
+const totalIncome = document.getElementById("total-income");
+const totalExpense = document.getElementById("total-expense");
+const netBalance = document.getElementById("net-balance");
+
